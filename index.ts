b67e8e3d6e03af6708e1f712df3525bba0bfc496
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2018-04-18 18:22:47 +0700

    initialize

---
+++ index.ts
@@ -0,0 +1,23 @@
+import * as express from 'express'
+import Chatbot from './src/chatbot'
+import bodyParser from 'body-parser'
+
+const app       = express.default()
+const chatbot   = new Chatbot()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/webhook', chatbot.middleware, (req: express.Request, res: express.Response) => {
+  chatbot.webhook(req, res)
+})
+
+app.use('/gitlab', chatbot.pushNotification)
+
+app.use('/ping', (req: express.Request, res: express.Response) => {
+  return res.json({ pong: 1 })
+})
+
+app.listen(process.env.PORT || 7581, () => {
+  console.log(`Server listen at port ${process.env.port || 7581}...`)
+})
---
+++ src/chatbot.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv'
+import * as express from 'express'
+import * as line from '@line/bot-sdk'
+
+
+dotenv.config()
+
+export default class {
+
+  public client: line.Client = new line.Client({ channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string })
+  public middleware: any     = line.middleware({ channelSecret: process.env.CHANNEL_SECRET as string })
+
+  public webhook(req: express.Request, res: express.Response) {
+    Promise
+      .all(req.body.events.map(async (event: line.WebhookEvent) => {
+        console.log(event)
+        if (event.type === 'message' && event.message.type === 'text' && event.message.text.toLowerCase() === '/ping') {
+          return this.client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: JSON.stringify(event)
+          })
+        }
+        return Promise.resolve(null)
+      }))
+      .then((result: any) => res.json(result))
+      .catch((err: any) => console.log(err))
+  }
+
+  public pushNotification(req: express.Request, res: express.Response) {
+    console.log(req.body)
+  }
+}
